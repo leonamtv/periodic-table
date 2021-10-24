faa6604465877f9e428f3b11cd83a920e31df29b
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2021-10-24 17:13:15 -0300

    Subindo footer

--- periodic-table/deploy.sh
+++ periodic-table/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ng build --prod=true --outputPath=dist --baseHref=leonamtv.github.io/periodic-table; cd dist; npx angular-cli-ghpages --dir="/" 
+ng build --prod=true --output-path dist --base-href /periodic-table/;
+npx angular-cli-ghpages --dir="dist";
+
